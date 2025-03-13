19a72345f4fa841c3792f75b646719b7c5411b98
Benfau1 <mslb38@gmail.com>
2025-03-13 12:33:44 +0100

    Init pipelines

---
+++ src/kedro_lm_bf/pipelines/deploy/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'deploy'
+generated using Kedro 0.19.11
+"""
---
+++ src/kedro_lm_bf/pipelines/deploy/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'deploy'
+generated using Kedro 0.19.11
+"""
+
+from kedro.pipeline import node, Pipeline, pipeline  # noqa
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
