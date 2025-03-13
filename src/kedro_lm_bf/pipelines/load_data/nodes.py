f80187bd6150a2ff39c438fbf8a72bd6060a077f
Benfau1 <mslb38@gmail.com>
2025-03-13 12:59:07 +0100

    Work on 1st pipeline

--- src/kedro_lm_bf/pipelines/load_data/nodes.py
+++ src/kedro_lm_bf/pipelines/load_data/nodes.py
@@ -1,4 +1,35 @@
-"""
-This is a boilerplate pipeline 'load_data'
-generated using Kedro 0.19.11
-"""
+import pandas as pd
+
+
+
+
+
+
+
+
+
+
+
+
+
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Nettoie les données en supprimant les valeurs aberrantes et interpolant les valeurs manquantes.
+
+    Args:
+        data (pd.DataFrame): Jeu de données brut.
+
+    Returns:
+        pd.DataFrame: Jeu de données nettoyé.
+    """
+
+    # Suppression des valeurs aberrantes
+    Q1 = data.quantile(0.25)
+    Q3 = data.quantile(0.75)
+    IQR = Q3 - Q1
+    data_cleaned = data[~((data < (Q1 - 1.5 * IQR)) | (data > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+    # Interpolation des valeurs manquantes
+    data_cleaned = data_cleaned.interpolate(method="linear")
+
+    return data_cleaned
--- src/kedro_lm_bf/pipelines/load_data/pipeline.py
+++ src/kedro_lm_bf/pipelines/load_data/pipeline.py
@@ -1,10 +1,14 @@
-"""
-This is a boilerplate pipeline 'load_data'
-generated using Kedro 0.19.11
-"""
+from kedro.pipeline import node, Pipeline, pipeline
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from kedro_lm_bf.pipelines.load_data.nodes import clean_data  # noqa
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+                func=clean_data,
+                inputs="raw_data",
+                outputs="cleaned_data",
+                name="data_cleaning_node",
+            ),
+    ])
