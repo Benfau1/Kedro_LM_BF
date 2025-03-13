4cad1e2287618f4f7834d2b3ac39d0d777d31461
Ludovic MARION <ludovicmarion70@gmail.com>
2025-03-13 15:01:22 +0100

    TestGeneration

--- src/kedro_lm_bf/pipelines/load_data/pipeline.py
+++ src/kedro_lm_bf/pipelines/load_data/pipeline.py
@@ -1,13 +1,18 @@
 from kedro.pipeline import node, Pipeline, pipeline
-
 from kedro_lm_bf.pipelines.load_data.nodes import clean_data  # noqa
+from .nodes import generate_audiogram_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
+                func=generate_audiogram_data, 
+                inputs=None, 
+                outputs="generated_csv", 
+                name="generate_audiograms"),
+        node(
                 func=clean_data,
-                inputs="raw_data",
+                inputs="generated_csv",
                 outputs="cleaned_data",
                 name="data_cleaning_node",
             ),
