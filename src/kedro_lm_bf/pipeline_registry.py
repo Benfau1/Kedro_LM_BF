6c8183dae90066beeab405bfe9753016e73f1c6d
Ludovic MARION <ludovicmarion70@gmail.com>
2025-04-09 16:46:18 +0200

    modif pipeline_registry

--- src/kedro_lm_bf/pipeline_registry.py
+++ src/kedro_lm_bf/pipeline_registry.py
@@ -6,6 +6,7 @@ from kedro.pipeline import Pipeline
 from kedro_lm_bf.pipelines.load_data import pipeline as load_data_pipeline
 from kedro_lm_bf.pipelines.train_data import pipeline as train_data_pipeline
 from kedro_lm_bf.pipelines.prediction import pipeline as prediction_pipeline
+from kedro_lm_bf.pipelines.transform_data import pipeline as transform_data_pipeline
 from kedro_lm_bf.pipelines.deploy import pipeline as deploy_pipeline
 
 def register_pipelines() -> dict[str, Pipeline]:
@@ -17,10 +18,10 @@ def register_pipelines() -> dict[str, Pipeline]:
     pipelines = find_pipelines()
 
     pipelines["load_data"] = load_data_pipeline.create_pipeline()
-    pipelines["evaluation"] = train_data_pipeline.create_pipeline()
+    pipelines["transform_data"] = transform_data_pipeline.create_pipeline()
+    pipelines["entrainement"] = train_data_pipeline.create_pipeline()
     pipelines["prediction"] = prediction_pipeline.create_pipeline()
     pipelines["deployment"] = deploy_pipeline.create_pipeline()
-
     pipelines["__default__"] = sum(pipelines.values())
 
     return pipelines
