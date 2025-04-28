3f1a640953cd4fec91a40ab4316b7c9d62d78cef
Ludovic MARION <ludovicmarion70@gmail.com>
2025-04-28 10:10:35 +0200

    correctif prediction

--- src/kedro_lm_bf/pipelines/prediction/nodes.py
+++ src/kedro_lm_bf/pipelines/prediction/nodes.py
@@ -3,8 +3,18 @@ import numpy as np
 from sklearn.metrics import r2_score
 
 
-def predict(data_to_predict: pd.DataFrame, trained_model) :
-    predictions = trained_model.predict(data_to_predict)
+def predict(data_to_predict: pd.DataFrame, trained_model):
+    # Ne garder que les colonnes "before"
+    before_columns = [col for col in data_to_predict.columns if "before" in col]
+    x_to_predict = data_to_predict[before_columns]
+
+    # Adapter la forme pour le modèle
+    x_to_predict = np.expand_dims(x_to_predict.values, axis=-1)
+
+    # Faire la prédiction
+    predictions = trained_model.predict(x_to_predict)
+
+    # Créer un DataFrame pour les prédictions
     predictions_df = pd.DataFrame(predictions, 
                                   columns=[
                                       "after_exam_125_Hz",
@@ -15,4 +25,5 @@ def predict(data_to_predict: pd.DataFrame, trained_model) :
                                       "after_exam_4000_Hz",
                                       "after_exam_8000_Hz"])
 
+    # Retourner les données originales + les prédictions
     return pd.concat([data_to_predict, predictions_df], axis=1)
